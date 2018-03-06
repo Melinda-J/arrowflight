e052905b5ea1fc5f73db17e25f35730f5479457e
MelindaJ <734674571@qq.com>
2018-03-06 17:22:16 +0800

    modify

--- sort.php
+++ sort.php
@@ -39,13 +39,13 @@ function selectSort($arr, $desc = false)
     if (!is_array($arr) || count($arr) == 0) {
         return $arr;
     }
-    // 外层循环，将剩余未排序数组元素中值最小的元素移至首位
+    // 外层循环，假定元素$arr[$i]为最小元素
     for ($i = 0, $size = count($arr); $i < $size - 1; $i++) {
-        // 假定第一位元素值最小，并保存索引到$p
+        // 保存最小元素索引到$p
         $p = $i;
-        // 内层循环，得到剩余未排序数组元素中值最小的元素的索引
+        // 内层循环，将元素$arr[$j]与最小元素进行比较
         for ($j = $i + 1; $j < $size; $j++) {
-            // 将假定最小元素值与下一位元素值进行比较，保存较小元素的索引到$p
+            // 若元素$arr[$j]小于最小元素，继续假定$arr[$j]为最小元素，保存索引到$p
             if ($arr[$j] < $arr[$p]) {
                 $p = $j;
             }
@@ -75,9 +75,24 @@ function insertSort($arr, $desc = false)
     if (!is_array($arr) || count($arr) == 0) {
         return $arr;
     }
-    for ($i = 0, $size = count($arr); $i < $size - 1; $i++) {
-        
+    // 外层循环，待插入元素$arr[$i]
+    for ($i = 1, $size = count($arr); $i < $size; $i++) {
+        // 待插入元素
+        $tmp = $arr[$i];
+        // 内层循环，待比较元素$arr[$j]
+        for ($j = $i - 1; $j >= 0; $j--) {
+            // 若待插入元素小于待比较元素，交换两元素位置
+            if ($tmp > $arr[$j]) {
+                break;
+            }
+            $arr[$j + 1] = $arr[$j];
+            $arr[$j]     = $tmp;
+        }
     }
+    if ($desc) {
+        return array_reverse($arr);
+    }
+    return $arr;
 }
 
 /**
@@ -95,7 +110,7 @@ function quickSort($arr, $desc = false)
     $base     = $arr[0];
     $frontArr = array();
     $backArr  = array();
-    // 将剩余元素与基准元素进行比较，大于基准的元素和小于基准的元素分开存储
+    // 将剩余元素与基准元素进行比较，大于基准的元素和小于基准的元素分别存储再$backArr和$frontArr中
     for ($i = 1, $size = count($arr); $i < $size; $i++) {
         if ($arr[$i] < $base) {
             $frontArr[] = $arr[$i];
@@ -103,7 +118,7 @@ function quickSort($arr, $desc = false)
             $backArr[] = $arr[$i];
         }
     }
-    // 递归
+    // 递归的对$frontArr和$backArr执行上述操作
     $func     = __FUNCTION__;
     $frontArr = $func($frontArr, $desc);
     $backArr  = $func($backArr, $desc);
@@ -112,4 +127,84 @@ function quickSort($arr, $desc = false)
         return array_merge($backArr, array($base), $frontArr);
     }
     return array_merge($frontArr, array($base), $backArr);
+}
+
+/**
+ * 二分查找(非递归)
+ * @param  mixed   $search 查找目标值
+ * @param  array   $arr    待检索的数组
+ * @param  integer $start  起始查找位置
+ * @param  integer $end    终止查找位置
+ * @return integer         目标值在待检索数组中的位置
+ */
+function divideSearch($search, $arr, $start = null, $end = null)
+{
+    if (!is_array($arr) || count($arr) == 0) {
+        return -1;
+    }
+    // 起始查找位置
+    if (!isset($start)) {
+        $start = 0;
+    }
+    // 终止查找位置
+    if (!isset($end)) {
+        $end = count($arr) - 1;
+    }
+    while ($end >= $start) {
+        // 取数组中间元素索引
+        $middle = floor(($start + $end) / 2);
+        if ($search > $arr[$middle]) {
+            // 若目标值大于中间元素，向后查找
+            $start = $middle + 1;
+        } else if ($search < $arr[$middle]) {
+            // 若目标值小于中间元素，向前查找
+            $end = $middle - 1;
+        } else {
+            // 若目标值等于中间元素，返回中间元素索引
+            return $middle;
+        }
+    }
+    return -1;
+}
+
+/**
+ * 二分查找(递归)
+ * @param  mixed $search  查找的目标值
+ * @param  array $arr     待检索的数组
+ * @param  integer $start 起始查找位置
+ * @param  integer $end   终止查找位置
+ * @return integer        目标值在待检所数组中的位置
+ */
+function halfSearch($search, $arr, $start = null, $end = null)
+{
+    if (!is_array($arr) || count($arr) == 0) {
+        return -1;
+    }
+    // 起始查找位置
+    if (!isset($start)) {
+        $start = 0;
+    }
+    // 终止查找位置
+    if (!isset($end)) {
+        $end = count($arr) - 1;
+    }
+    if ($end < $start) {
+        return -1;
+    }
+    // 中间元素索引
+    $middle = floor(($start + $end) / 2);
+    $func   = __FUNCTION__;
+    if ($search > $arr[$middle]) {
+        // 若目标值大于中间元素，向后查找
+        $start = $middle + 1;
+        return $func($search, $arr, $start, $end);
+    } else if ($search < $arr[$middle]) {
+        // 若目标值小于中间元素，向前查找
+        $end = $middle - 1;
+        return $func($search, $arr, $start, $end);
+    } else {
+        // 若目标值等于中间元素，返回中间元素索引
+        return $middle;
+    }
+    return -1;
 }

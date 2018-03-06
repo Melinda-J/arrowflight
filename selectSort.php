9fb8bfe61c1a50e3f033a131e8093c22ede78528
MelindaJ <734674571@qq.com>
2018-03-06 17:56:08 +0800

    Add selectSort.php

---
+++ selectSort.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * 选择排序
+ * @param  array   $arr  将要排序的数组
+ * @param  boolean $desc 是否倒序
+ * @return array         排序后的数组
+ */
+function selectSort($arr, $desc = false)
+{
+    if (!is_array($arr) || count($arr) == 0) {
+        return $arr;
+    }
+    // 外层循环，假定元素$arr[$i]为最小元素
+    for ($i = 0, $size = count($arr); $i < $size - 1; $i++) {
+        // 保存最小元素索引到$p
+        $p = $i;
+        // 内层循环，将元素$arr[$j]与最小元素进行比较
+        for ($j = $i + 1; $j < $size; $j++) {
+            // 若元素$arr[$j]小于最小元素，继续假定$arr[$j]为最小元素，保存索引到$p
+            if ($arr[$j] < $arr[$p]) {
+                $p = $j;
+            }
+        }
+        // 若假定最小元素索引不等于实际最小元素索引，交换两元素位置
+        if ($p == $i) {
+            continue;
+        }
+        $tmp     = $arr[$i];
+        $arr[$i] = $arr[$p];
+        $arr[$p] = $tmp;
+    }
+    if ($desc) {
+        return array_reverse($arr);
+    }
+    return $arr;
+}

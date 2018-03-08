605c0e077089065c40b7abf3a5094586e557200a
MelindaJ <734674571@qq.com>
2018-03-08 15:20:12 +0800

    update

--- src/bubbleSort.php
+++ src/bubbleSort.php
@@ -6,11 +6,12 @@
  */
 function bubbleSort(&$arr)
 {
-    if (!is_array($arr) || count($arr) <= 1) {
+    $size = count($arr);
+    if ($size <= 1) {
         return;
     }
     // 外层循环，将剩余未排序数组元素中值最大的元素移至末位
-    for ($i = 0, $size = count($arr); $i < $size - 1; $i++) {
+    for ($i = 0; $i < $size - 1; $i++) {
         // 内层循环，若当前元素的值比下一位元素值大，交换两元素位置
         for ($j = 0; $j < $size - $i - 1; $j++) {
             if ($arr[$j] > $arr[$j + 1]) {

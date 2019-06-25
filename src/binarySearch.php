b2c74b1d31f3d36fc25ec255518fcfb81019868f
dengj <dengjing@think-land.com>
2019-06-25 09:16:13 +0800

    simplify

--- src/binarySearch.php
+++ src/binarySearch.php
@@ -11,9 +11,6 @@
 function divideSearch($search, $arr, $start = null, $end = null)
 {
     $size = count($arr);
-    if ($size == 0) {
-        return -1;
-    }
     // 起始查找位置
     if (is_null($start)) {
         $start = 0;
@@ -22,22 +19,20 @@ function divideSearch($search, $arr, $start = null, $end = null)
     if (is_null($end)) {
         $end = $size - 1;
     }
+    $index = -1;
     while ($end >= $start) {
         // 取数组中间元素索引
         $middle = intval(($start + $end) / 2);
-        if ($search > $arr[$middle]) {
-            // 若目标值大于中间元素，向后查找
-            $start = $middle + 1;
-        } else if ($search < $arr[$middle]) {
-            // 若目标值小于中间元素，向前查找
-            $end = $middle - 1;
-        } else {
-            // 若目标值等于中间元素，返回中间元素索引
-            return $middle;
+        // 若目标值等于中间元素，返回中间元素索引
+        if ($search == $arr[$middle]) {
+            $index = $middle;
+            break;
         }
+        // 若目标值大于中间元素，向后查找|若目标值小于中间元素，向前查找
+        $search > $arr[$middle] ? $start = $middle + 1 : $end = $middle - 1;
     }
 
-    return -1;
+    return $index;
 }
 
 /**
@@ -68,18 +63,12 @@ function halfSearch($search, $arr, $start = null, $end = null)
     }
     // 中间元素索引
     $middle = intval(($start + $end) / 2);
-    if ($search > $arr[$middle]) {
-        // 若目标值大于中间元素，向后查找
-        $start = $middle + 1;
-
-        return call_user_func_array(__FUNCTION__, [$search, $arr, $start, $end]);
-    } else if ($search < $arr[$middle]) {
-        // 若目标值小于中间元素，向前查找
-        $end = $middle - 1;
-
-        return call_user_func_array(__FUNCTION__, [$search, $arr, $start, $end]);
-    } else {
-        // 若目标值等于中间元素，返回中间元素索引
+    // 若目标值等于中间元素，返回中间元素索引
+    if ($search == $arr[$middle]) {
         return $middle;
     }
+    // 若目标值大于中间元素，向后查找|若目标值小于中间元素，向前查找
+    $search > $arr[$middle] ? $start = $middle + 1 : $end = $middle - 1;
+
+    return call_user_func_array(__FUNCTION__, [$search, $arr, $start, $end]);
 }
